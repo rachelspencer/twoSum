a54d014c458d4a8ee0141a369016c54440cb605d
Rachel Spencer <rachelspencer688@gmail.com>
2023-12-06 13:43:33 -0500

    initial commit

---
+++ twoSum.js
@@ -0,0 +1,6 @@
+// takes an array of nums and an integer target
+//return indicies of the two nums that add up to target
+
+// assume: each input would have exactly one solution
+// assume: you may not use the same um twice
+// return the answer in any order
